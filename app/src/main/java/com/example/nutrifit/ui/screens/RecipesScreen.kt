c2ecf28d5f68b5616b3f4cc6f42282c290fd9b70
Carlos <Carlosestebancarretero.ovb@gmail.com>
2024-04-21 23:02:22 +0200

    Añadimos nuevos iconos y mejoramos la pantalla de RecipesScreen.kt, nose como poner el color origina de los iconos de esta pantalla, buen trabajo compañeros sois los mejores.

--- app/src/main/java/com/example/nutrifit/ui/screens/RecipesScreen.kt
+++ app/src/main/java/com/example/nutrifit/ui/screens/RecipesScreen.kt
@@ -1,22 +1,19 @@
 package com.example.nutrifit.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.nutrifit.R
 
 data class Meal(
     val name: String,
@@ -36,7 +33,7 @@ data class DayMealPlan(
 )
 
 @Composable
-fun MealPlanScreen(dayMealPlans: List<DayMealPlan>) {
+fun RecipesScreen(dayMealPlans: List<DayMealPlan>) {
     LazyColumn {
         items(dayMealPlans) { dayMealPlan ->
             DayMealPlanCard(dayMealPlan)
@@ -46,98 +43,100 @@ fun MealPlanScreen(dayMealPlans: List<DayMealPlan>) {
 
 @Composable
 fun DayMealPlanCard(dayMealPlan: DayMealPlan) {
-    Surface(
-        modifier = Modifier.padding(8.dp),
-        color = MaterialTheme.colorScheme.background
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column {
             Text(
                 text = dayMealPlan.dayOfWeek,
                 style = MaterialTheme.typography.headlineMedium,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            MealCard("Desayuno", dayMealPlan.breakfast)
-            MealCard("Comida", dayMealPlan.lunch)
-            MealCard("Merienda", dayMealPlan.snack)
-            MealCard("Cena", dayMealPlan.dinner)
+            MealCard("Desayuno", dayMealPlan.breakfast, R.drawable.ic_desayuno)
+            MealCard("Comida", dayMealPlan.lunch, R.drawable.ic_comida)
+            MealCard("Merienda", dayMealPlan.snack, R.drawable.ic_meriendas)
+            MealCard("Cena", dayMealPlan.dinner, R.drawable.ic_cena)
         }
     }
 }
 
 @Composable
-fun MealCard(mealType: String, meal: Meal) {
+fun MealCard(mealType: String, meal: Meal, iconId: Int) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp),
-        // elevation = 4.dp
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = "$mealType: ${meal.name}",
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold
+        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            // Aquí se quita cualquier modificador de color para mantener el color original del ícono
+            Icon(
+                painter = painterResource(id = iconId),
+                contentDescription = "$mealType icon",
+                modifier = Modifier.size(60.dp)
             )
-            Text(
-                text = meal.description,
-                style = MaterialTheme.typography.bodyLarge
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Calorías: ${meal.calories} kcal")
-            Text(text = "Proteínas: ${meal.protein}g")
-            Text(text = "Grasas: ${meal.fat}g")
-            Text(text = "Carbohidratos: ${meal.carbs}g")
+            Spacer(Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = "$mealType: ${meal.name}",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = meal.description,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(text = "Calorías: ${meal.calories} kcal")
+                Text(text = "Proteínas: ${meal.protein}g")
+                Text(text = "Grasas: ${meal.fat}g")
+                Text(text = "Carbohidratos: ${meal.carbs}g")
+            }
         }
     }
 }
 
-// Sample data for preview
-val sampleDayMealPlans = listOf(
-    DayMealPlan(
-        dayOfWeek = "Lunes",
-        breakfast = Meal(
-            name = "Tortilla española",
-            description = "Con 2 huevos, 50 g de patata y 30 g de cebolla",
-            calories = 250,
-            protein = 14,
-            fat = 15,
-            carbs = 15
-        ),
-        lunch = Meal(
-            name = "Ensalada mixta ",
-            description = "con 150 g de pechuga de pollo a la plancha, 30 g de lechuga, 50 g de tomate, 40 g de pepino y 30 g de queso fresco.",
-            calories = 300,
-            protein = 35,
-            fat = 7,
-            carbs = 10
-            // ... define los datos para el almuerzo
-        ),
-        snack = Meal(
-            name = "Yoghurt natural con almendras",
-            description = "Yogurt natural (125 g) con 20 g de almendras",
-            calories = 200,
-            protein = 12,
-            fat = 10,
-            carbs = 15
-            // ... define los datos para la merienda
-        ),
-        dinner = Meal(
-            name = "Bacalao al horno con brocoli y arroz integral",
-            description = "Bacalao al horno (150 g) con 200 g de brócoli al vapor y 50 g de arroz integral",
-            calories = 350,
-            protein = 32,
-            fat = 5,
-            carbs = 40
-            // ... define los datos para la cena
-        )
+// Dummy data for the preview
+private val sampleMeals = DayMealPlan(
+    dayOfWeek = "Lunes",
+    breakfast = Meal(
+        name = "Tortilla española",
+        description = "Con 2 huevos, 50 g de patata y 30 g de cebolla",
+        calories = 250,
+        protein = 14,
+        fat = 15,
+        carbs = 15
+    ),
+    lunch = Meal(
+        name = "Ensalada mixta",
+        description = "Con 150 g de pechuga de pollo a la plancha, lechuga, tomate, pepino y queso fresco",
+        calories = 350,
+        protein = 30,
+        fat = 10,
+        carbs = 20
+    ),
+    snack = Meal(
+        name = "Yogur natural con almendras",
+        description = "Yogur natural con un puñado de almendras",
+        calories = 200,
+        protein = 10,
+        fat = 15,
+        carbs = 5
+    ),
+    dinner = Meal(
+        name = "Bacalao al horno con brócoli",
+        description = "Bacalao al horno con brócoli al vapor y arroz integral",
+        calories = 300,
+        protein = 25,
+        fat = 5,
+        carbs = 35
     )
-    // ... define el resto de los días
 )
 
+@Preview(showBackground = true)
 @Composable
-@Preview
-fun MealPlanScreenPreview() {
-    MealPlanScreen(dayMealPlans = sampleDayMealPlans)
+fun RecipesScreenPreview() {
+    RecipesScreen(dayMealPlans = listOf(sampleMeals))
 }

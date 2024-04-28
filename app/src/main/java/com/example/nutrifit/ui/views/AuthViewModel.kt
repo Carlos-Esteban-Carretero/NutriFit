ded0b4b4fe52bbdd0bbf8a38774d764a3383aab3
Carlos-Esteban-Carretero <carlosestebancarretero.ovb@gmail.com>
2024-04-28 21:43:56 +0200

    realizando todo el proceso del Login, nos quedamos en el minuto 4:35 de https://www.youtube.com/watch?v=NFot9_bSFhw  (Video del Rober) gibran nos manda a otro video antes de continuar : https://www.youtube.com/watch?v=lC_mgTqiaSM Mañana trabajaremos sobre esto, dejo la APP funcionando y sin errores.

--- app/src/main/java/com/example/nutrifit/ui/views/AuthViewModel.kt
+++ app/src/main/java/com/example/nutrifit/ui/views/AuthViewModel.kt
@@ -1,20 +1,40 @@
 package com.example.nutrifit.ui.views
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 
 class AuthViewModel : ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+    private val _loading = MutableLiveData(false)
     // Inicializa Firebase Auth, etc.
 
-    fun signIn(email: String, password: String) {
-        // Lógica para iniciar sesión con Firebase Auth
+    fun signInWhitEmailAndPassword(email: String, password: String, home: () -> Unit)
+        = viewModelScope.launch {
+            try {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("Mascota Feliz", "signInWithEmailAndPassword logueado!!")
+                            home()
+                        } else {
+                            Log.d(
+                                "Mascota Feliz",
+                                "signInWithEmailAndPassword: ${task.result.toString()}"
+                            )
+
+                        }
+                    }
+            } catch (ex: Exception) {
+                Log.d("Mascota Feliz", "signInWithEmailAndPassword: ${ex.message}")
+            }
+        }
     }
 
-    fun signUp(email: String, password: String) {
-        // Lógica para registrar un nuevo usuario con Firebase Auth
-    }
 
-    fun signInWithGoogle() {
-        // Lógica para iniciar sesión con Google Sign-In y Firebase Auth
-    }
-}
 
